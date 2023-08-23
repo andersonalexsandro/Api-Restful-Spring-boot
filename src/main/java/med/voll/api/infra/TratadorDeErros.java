e35fff1189b252f4958c08391c6cade396dc522d
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-08-22 21:53:25 -0300

    erro 400 indicando erros de entrada implementado

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,9 +2,13 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -12,5 +16,17 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception){
+        List<FieldError> errorFieldList = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errorFieldList.stream().map(ErrorValidationDTO::new).toList());
+    }
+
+    private record ErrorValidationDTO(String campo, String mensagem){
+        public ErrorValidationDTO(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
